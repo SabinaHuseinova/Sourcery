0564feccdc8fade6c68376bdf7f8dab9b79863fe
Artyom Y. Razinov <arazinov@avito.ru>
2021-10-13 17:38:22 +0300

    Update PathKit to support Xcode 13

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Commander", url: "https://github.com/kylef/Commander.git", .exact("0.9.1")),
-        .package(name: "PathKit", url: "https://github.com/kylef/PathKit.git", .branch("master")),
+        .package(name: "PathKit", url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
         .package(name: "SourceKitten", url: "https://github.com/jpsim/SourceKitten.git", from: "0.30.1"),
         .package(name: "StencilSwiftKit", url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.7.0"),
     ],
